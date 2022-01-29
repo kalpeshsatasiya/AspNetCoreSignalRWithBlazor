4cce6ece4a88821f2018f856bcde7dad43a4d78d
Kalpesh Satasiya <satasiya.kalpesh2006@gmail.com>
2022-01-29 16:36:34 +0530

    first commit

---
+++ Hubs/ProductHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace BlazorAppDemo.Hubs
+{
+    public class ProductHub : Hub
+    {
+        public async Task SendProduct(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveProduct", user, message);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,40 @@
+using BlazorAppDemo.Data;
+using BlazorAppDemo.Hubs;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.ResponseCompression;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddSingleton<WeatherForecastService>();
+builder.Services.AddResponseCompression(opts =>
+{
+    opts.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(
+        new[] { "application/octet-stream" });
+});
+
+var app = builder.Build();
+app.UseResponseCompression();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapHub<ProductHub>("/producthub");
+app.MapFallbackToPage("/_Host");
+
+app.Run();
